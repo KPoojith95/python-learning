691b2b487b159128ab7dbb49afc1719da0547d2e
K Poojith <kpoojith95@gmail.com>
2026-07-06 00:40:38 +0530

    Added Personal Profile Generator Project

---
+++ fav things.py
@@ -0,0 +1,10 @@
+favorite_anime = "Dragon Ball"
+favorite_place = "new york"
+favorite_song = "heat waves"
+favorite_sports = "football"
+bestfriend = "no one"
+print("my favorite anime is:", favorite_anime)
+print("my favorite place is:", favorite_place)  
+print("my favorite song is:", favorite_song)
+print("my favorite sports is:", favorite_sports)
+print("my bestfriend is:", bestfriend)
---
+++ variables.py
@@ -0,0 +1,11 @@
+name = "K Poojith"
+age = 18
+college = "PES University"
+course = "BCA"
+dream = "Cybersecurity Engineer"
+
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Course:", course)
+print("Dream:", dream)
